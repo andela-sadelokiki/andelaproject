1cfa1efbbe9093a016ce1af9e514ebc435c9e031
Adebayo Oluwadamilola <bola3624@gmail.com>
2014-08-13 05:26:37 +0100

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//code fo the menu
+// $(document).ready(function(){
+// 	$('.menu > li').bind('mouseover', openSubMenu);
+// 		function openSubMenu(){
+// 			$(this).find('menu li ul').css('visibility', 'visible');
+// 		};
+// });
+
+
+$(function(){
+
+    $("ul.menu li").hover(function(){
+    
+        //$(this).addClass("hover");
+        $('ul:first',this).css('visibility', 'visible');
+    
+    }, function(){
+    
+        //$(this).removeClass("hover");
+        $('ul:first',this).css('visibility', 'hidden');
+    
+    });
+    
+    //$("ul.dropdown li ul li:has(ul)").find("a:first").append(" &raquo; ");
+
+});
